aa01c96afb137ccd7b3ece483dd6ce38c33f1aae
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-11-29 17:47:51 +0800

    Include application.css starter

--- base_ui/template.rb
+++ base_ui/template.rb
@@ -1,5 +1,13 @@
-run 'yarn add tailwindcss'
+# install tailwindcss
+# tailwindcss@2.x requires PostCSS 8, which doesn't come with Rails at this time. Feel free to change this!
+run 'yarn add tailwindcss@1.9.6'
 run 'npx tailwindcss init'
 insert_into_file("postcss.config.js",
                  "    require('tailwindcss'),\n    require('autoprefixer'),\n",
                  after: "require('postcss-import'),\n")
+
+# add our templates
+def source_paths
+  [__dir__]
+end
+directory 'app'
