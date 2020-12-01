607a31285bf0f076885a55ff0f90829cf17be2c2
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-01 23:13:33 +0800

    Naively add the Inter font

---
+++ base_ui/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
