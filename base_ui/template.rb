bcd65d76c2acc6f21413bb5e433ac0c821254943
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-06 12:47:34 +0800

    Remove scaffold stylesheets from generators (actually all CSS)

--- base_ui/template.rb
+++ base_ui/template.rb
@@ -6,6 +6,13 @@ insert_into_file("postcss.config.js",
                  "    require('tailwindcss'),\n    require('autoprefixer'),\n",
                  after: "require('postcss-import'),\n")
 
+# Stop generating scaffold.css. This might be a bit heavy-handed, so you may want to change this.
+application (<<~RUBY)
+  config.generators do |g|
+    g.stylesheets false
+  end
+RUBY
+
 def source_paths
   [__dir__]
 end
