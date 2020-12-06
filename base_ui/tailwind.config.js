2db753ab078a0cf4bd304b0ebee25bcbb9ab0aed
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-12-06 12:46:52 +0800

    Remove inter font

--- base_ui/tailwind.config.js
+++ base_ui/tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
     },
   },
   variants: {},
